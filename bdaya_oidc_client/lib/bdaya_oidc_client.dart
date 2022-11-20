8e3bbbfa8b5a49a93669861f9d0a2f0fd7e6937d
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2022-11-20 12:12:32 +0200

    Initial upload

--- bdaya_oidc_client/lib/bdaya_oidc_client.dart
+++ bdaya_oidc_client/lib/bdaya_oidc_client.dart
@@ -1,8 +1,1 @@
-
-import 'bdaya_oidc_client_platform_interface.dart';
-
-class BdayaOidcClient {
-  Future<String?> getPlatformVersion() {
-    return BdayaOidcClientPlatform.instance.getPlatformVersion();
-  }
-}
+export 'src/client.dart';
--- bdaya_oidc_client_android/CHANGELOG.md
+++ bdaya_oidc_client_android/CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.1
+## 0.0.1-alpha 20-Nov-2022
 
-* TODO: Describe initial release.
+* Initial push
